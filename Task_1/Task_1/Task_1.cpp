fa96f364ad6e6c88c0752a0f66c618394bfd7783
Tatyana Mezentseva <62556610+JackieUniverse@users.noreply.github.com>
2022-08-01 11:48:08 +0300

    Add files via upload

---
+++ Task_1/Task_1/Task_1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm> 
+using namespace std;
+
+string encodeDuplicate(string s) {
+
+    string ans = "";
+
+    for (int i = 0; i < s.size(); i++) {
+
+        s[i] = tolower(s[i]);
+
+    }
+
+    for (char ch : s) {
+        if (count(s.begin(), s.end(), ch) > 1)
+            ans += ")";
+        else
+            ans += "(";
+    }
+
+    return ans;
+}
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int length;
+    string str;
+
+    cout << "Введите строку: ";
+    getline(cin, str);
+
+    cout << "Видоизмененная строка: ";
+
+    cout << encodeDuplicate(str);
+
+}
+
+
